32c825ee15c0c22ad3e3b74caa96bea67e3b2e77
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-12 19:47:19 +0300

    connect to the database and install mongodb

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const PORT = 3000
 const path = require('path')
 const app = express();
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://zubalana0:xs2xO7q9rBvKBrdy@root.iqk47mq.mongodb.net/?retryWrites=true&w=majority&appName=root`)
+.then(()=>{
+    console.log(`Connected to mongo DB`)
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req,res)=>{
     res.sendFile(__dirname, 'public', 'index.html')
