ccade513cbe6b9a5b2d5e08187eeaf03db1c4cc9
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-12 21:46:05 +0300

    create the server side excuses creation endpoint

--- app.js
+++ app.js
@@ -6,13 +6,29 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 dotenv.config();
 
+//database connection 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+const Excuses = mongoose.model('Excuses', {author: String, excuse: String})
 
+
+//new excuses creating
+app.post('/add-excuse', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { author, excuse } = req.body;
+        const excuses = new Excuses({ author, excuse });
+        await excuses.save();
+        console.log(`Excuse created`);
+        res.status(201).json(excuses);
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req,res)=>{
