12335473c5f3cd6d151140e3688e7a0df8b1713c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-12 21:13:34 +0300

    connect to the database and fix the connection bug

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const PORT = 3000
 const path = require('path')
 const app = express();
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://zubalana0:xs2xO7q9rBvKBrdy@root.iqk47mq.mongodb.net/?retryWrites=true&w=majority&appName=root`)
-.then(()=>{
-    console.log(`Connected to mongo DB`)
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req,res)=>{
