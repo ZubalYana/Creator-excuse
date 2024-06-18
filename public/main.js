f8026d37aeaf96ecb76bb9896980a70a2bc1ff34
MarkivNazar <nazarmn2008@gmail.com>
2024-06-18 14:24:25 +0300

    style public

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 //random excuse
-$('.randomExcuse').click(() => {
+$('.generate').click(() => {
     axios.get('http://localhost:3000/excuses')
     .then((response) => {
         const excuses = response.data;
@@ -9,11 +9,22 @@ $('.randomExcuse').click(() => {
             const randomIndex = Math.floor(Math.random() * numExcuses);
             const randomExcuse = excuses[randomIndex].excuse;
             $('#excuse').text(randomExcuse);
+            alert('gf')
         } else {
             $('#excuse').text("No excuses available.");
+            alert('gf')
         }
     })
     .catch((error) => {
         console.error('Error fetching excuses:', error);
+        alert('gf')
     });
 });
+
+
+
+
+
+
+
+
