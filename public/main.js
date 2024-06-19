5897345975e4aed13189d1f7994daab207fc628a
MarkivNazar <nazarmn2008@gmail.com>
2024-06-19 19:16:00 +0300

    Date

--- public/main.js
+++ public/main.js
@@ -76,7 +76,10 @@ $(document).ready(function() {
         let year = now.getFullYear().toString().slice(-2); 
         let month = (now.getMonth() + 1).toString().padStart(2, '0'); 
         let day = now.getDate().toString().padStart(2, '0'); 
-        return `${day}.${month}.${year}`;
+        let hours = now.getHours().toString().padStart(2, '0'); 
+        let minutes = now.getMinutes().toString().padStart(2, '0'); 
+        let seconds = now.getSeconds().toString().padStart(2, '0');
+        return ` ${day}/${month}/${year} <div class = "time"> ${hours}:${minutes}:${seconds}</div>`;
     }
 
    
