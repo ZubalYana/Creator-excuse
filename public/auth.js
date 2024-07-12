6264f6f713f62aee46783ce3e042a3a4341193a5
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-12 09:23:21 +0300

    add the sign in container password eye functionality

--- public/auth.js
+++ public/auth.js
@@ -83,3 +83,17 @@ $('.showPassword').click(function() {
 
     }
 });
+$('.showPasswordSignIn').click(function() {
+    let passwordInput = $('#Sign_password');
+    let icon = $(this);
+
+    if (passwordInput.attr('type') === 'password') {
+        passwordInput.attr('type', 'text');
+        icon.removeClass('fa-eye').addClass('fa-eye-slash');
+
+    } else {
+        passwordInput.attr('type', 'password');
+        icon.removeClass('fa-eye-slash').addClass('fa-eye');
+
+    }
+});
